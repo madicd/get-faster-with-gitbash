ed15966fee4a4e1d2379cfa034db78e6d9ebd337
madicd <madicdjordje@gmail.com>
2018-03-08 18:35:18 +0100

    Removes checking credentials on login

--- src/main/java/com/madj/demo/AuthController.java
+++ src/main/java/com/madj/demo/AuthController.java
@@ -1,7 +1,5 @@
 package com.madj.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -16,34 +14,19 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @RestController
 public class AuthController {
 
-    private final UserService userService;
-
     private final TokenService tokenService;
 
     @Autowired
-    public AuthController(UserService userService, TokenService tokenService) {
-        this.userService = userService;
+    public AuthController(TokenService tokenService) {
         this.tokenService = tokenService;
     }
 
     @RequestMapping(path = "/login", method = POST)
     public ResponseEntity<String> login(@RequestHeader("Authorization") String authHeader) {
-        String credentials = credentialsFromBasicAuthHeader(authHeader);
-
-        if (!userService.validCredentials(credentials)) {
-            return new ResponseEntity<>("Bad credentials!", HttpStatus.BAD_REQUEST);
-        }
-
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set(X_DEMO_AUTHORIZATION_HEADER, tokenService.getToken());
-        ResponseEntity<String> response = new ResponseEntity<>("Santa, you're in!", responseHeaders, HttpStatus.ACCEPTED);
 
-        return response;
-    }
-
-    private String credentialsFromBasicAuthHeader(@RequestHeader("Authorization") String authHeader) {
-        String[] splitted = authHeader.split(" ");
-        return splitted[1];
+        return new ResponseEntity<>("Welcome!", responseHeaders, HttpStatus.ACCEPTED);
     }
 
 }
